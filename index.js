c01e4d90422da28148d533ff1bab7a2967cbab0b
Ishtiaq uddin <ishty119@gmail.com>
2022-05-04 21:10:42 +0600

    display product in client site

--- index.js
+++ index.js
@@ -10,55 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dg9iw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrk7y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
   try {
     await client.connect();
-    const serviceCollection = client.db("geniusCar").collection("service");
-    const orderCollection =client.db('geniusCar').collection('order');
+    const productCollection = client.db("laptopWarehouse").collection("product");
+    
 
-    app.get("/service", async (req, res) => {
+    app.get("/product", async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     });
 
-    app.get('/service/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id:ObjectId(id)};
-        const service = await serviceCollection.findOne(query);
-        res.send(service);
-    })
-
-    //POST 
-    app.post('/service',async(req,res)=>{
-        const newService = req.body;
-        const result = await serviceCollection.insertOne(newService);
-        res.send(result);
-    })
-
-    //DELETE
-    app.delete('/service/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await serviceCollection.deleteOne(query);
-        res.send(result);
-    });
-
-    //order collection api
-    app.post('/order',async(req,res)=>{
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result);
-    })
-
+    
 
   } finally {
   }
@@ -67,7 +35,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Running Genius Server");
+  res.send("Running laptop-warehouse Server");
 });
 
 app.listen(port, () => {
